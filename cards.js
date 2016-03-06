fef56954fd531c4ad3e767fc2c404aa85c9628cc
David Cramb <david.cramb@gmail.com>
2016-03-06 13:35:07 -0600

    refactoring code because I can't get card deletion to work with childNodes the way I was trying. New code properly adds event listeners per each new card delete button

--- cards.js
+++ cards.js
@@ -1,6 +1,6 @@
 var createButton = document.getElementById('create');
 var text = document.getElementById('textarea');
-var cardArea = document.getElementsByClassName('cardArea');
+var cardArea = document.getElementById('cardArea');
 
 
 function addCard () {
@@ -8,8 +8,38 @@ function addCard () {
     text = text.value
     return text
   };
-  cardArea[0].innerHTML += '<section class="card">'+ getText(text) + '</section>'  
+
+  cardArea.innerHTML += '<section class="card">'+ getText(text) + '<button class="del">delete</button>' + '</section>';
+  var singleCard = document.getElementsByClassName('card');
+  var delButton = document.getElementsByClassName('del');
+  for (var i = 0; i < singleCard.length; i++) {
+   delButton[i].addEventListener('click', delCard)
+  };
+  
+  //     // console.log(cardArea.removeChild(cardArea.lastChild));
+  //   for (var i = 0; i < cardArea.children.length; i++) {
+  //     var cards = cardArea.childNodes;
+  //     var cardToDelete = cardArea.childNodes[i];
+  //   };
+  //     // cardArea.removeChild(cardToDelete);
+  //     console.log(cardArea.removeChild(cardToDelete));
+  // };
+
+  // });
+  //  delButton[i].addEventListener('click', function(){
+  //   console.log(cardArea.childNodes);
+  // });
+    
+    
+
+
 };  
+  function delCard (e) {
+    console.log('e', e)
+    if (e.currentTarget.className === "del")
+      console.log(e)
+
+  };
 
 
 createButton.addEventListener('click', addCard, false);
